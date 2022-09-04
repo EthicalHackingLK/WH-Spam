f43b66cc4a0718e890cbb1d7625b2ee40ca82bea
Ethical Hacking LK <105837208+EthicalHackingLK@users.noreply.github.com>
2022-09-04 12:42:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ mylogo = f"""
         ▀▄▀▄▀ █▀█ ░░ ▄█ █▀▀ █▀█ █░▀░█
 
                        V.1.0\n\n"""[1:]                     
-make=CBox.Lines("Tool By Ethical Hacking LK")
+make=Box.Lines("Tool By Ethical Hacking LK")
 
 menu_note=Center.XCenter(Box.DoubleCube(
 """please choice a no..\n
